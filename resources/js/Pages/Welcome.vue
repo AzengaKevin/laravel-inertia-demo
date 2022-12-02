01f6ec37194858e21cc9d472e3e187a11de271f9
Azenga Kevin <azenga.kevin7@gmail.com>
2022-12-02 15:15:16 +0300

    WIP: Basic client and server side setup

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup>
+    defineProps({
+        appName: String
+    })
+</script>
+<template>
+    <header class="shadow-sm">
+        <nav class="navbar navbar-light navbar-expand-sm bg-light">
+            <div class="container-fluid">
+                <a href="/" class="navbar-brand">{{ appName }}</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container py-4">
+            <h1>Learning Laravel Inertia</h1>
+            <p class="lead">The modern monolith</p>
+        </div>
+    </main>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn ()  => inertia('Welcome', [
+    'appName' => config('app.name', 'Laravel With Inertia Demo')
+]))->name('welcome');
